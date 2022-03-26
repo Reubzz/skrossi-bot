9b51fe3c8b20043b784a8080816d12de54929042
Reubz <reugeorac@gmail.com>
2022-03-26 17:16:32 +0530

    Changed Activity Status

--- events/ready.js
+++ events/ready.js
@@ -3,6 +3,6 @@ const client = require("../index");
 client.on("ready", () => {
     console.log(`${client.user.tag} is up and ready to go!`)
 
-		client.user.setActivity('Valorant', {type: 'PLAYING' })
+		client.user.setActivity('VALORANT', {type: 'PLAYING' })
   	client.user.setStatus('idle')	
 });
