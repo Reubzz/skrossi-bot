eb2e44b28786d974e6599e7be982bed794dc4de2
Reubz <reugeorac@gmail.com>
2022-03-30 21:14:58 +0530

    Added Leaderboard Images

--- SlashCommands/info/Leveling System/leaderboard.js
+++ SlashCommands/info/Leveling System/leaderboard.js
@@ -1,6 +1,8 @@
 const xp = require('simply-xp');
 const simplydjs = require('simply-djs')
-const { Client, CommandInteraction, MessageActionRow, MessageButton, MessageEmbed } = require('discord.js');
+const { Client, CommandInteraction, MessageActionRow, MessageButton, MessageAttachment, MessageEmbed } = require('discord.js');
+const canvas = require('discord-canvas')
+
 
 module.exports = {
     name: 'leaderboard',
@@ -13,39 +15,65 @@ module.exports = {
      */
 
     run: async (client, interaction, args) => {
-        await xp.leaderboard(client, interaction.guild.id, 45).then(board => {
+        await xp.leaderboard(client, interaction.guild.id, 45).then(async board => {
             let a = []
             let b = []
-						let c = []
+			let c = []
          
             board.forEach(user => {
-                if (user.position <= 10) {
-                    a.push(`@ ${user.username}   • •   'Lvl: ${user.level}'\n--------------------------------------`)
+                let u = interaction.guild.members.cache.find((x) => x.id === user.userID)
+                if (user.position <= 11) {
+                    a.push({
+                        "icon": `${u.displayAvatarURL({ format: 'png' })}`,
+                        "data": `${user.username}   • •   LVL: ${user.level}`
+                    })
                 } else if (user.position > 10 && user.position <= 20) {
-                    b.push(`@ ${user.username}   • •   'Lvl: ${user.level}'\n--------------------------------------`)
+                    b.push({
+                        "icon": `${u.displayAvatarURL({ format: 'png' })}`,
+                        "data": `${user.username}   • •   LVL: ${user.level}`
+                    })
                 } else if (user.position > 20 && user.position <= 30) {
-                    c.push(`@ ${user.username}   • •   'Lvl: ${user.level}'\n--------------------------------------`)
+                    c.push({
+                        "icon": `${u.displayAvatarURL({ format: 'png' })}`,
+                        "data": `${user.username}   • •   LVL: ${user.level}`
+                    })
                 }
             })
+
+            const image = new canvas.Leaderboard()
+                .setData(a)
+                .setAvatars(true)
+                .toAttachment()
+
+            const attachment = new MessageAttachment((await image).toBuffer(), 'leaderboard-1.png');
+
          
             let emb = new MessageEmbed()
-                .setTitle('Leaderboard')
-                .setDescription(`***1 - 10 Users*** **leaderboard**\n\`\`\`py\n${a.toString().replaceAll(',', '\n')}\n\`\`\``)
+                .setTitle(`${interaction.guild.name}'s Leaderboard`)
+                .setDescription(`Full Leaderboard top 100 users - *Coming Soon*`)
                 .setColor('#2f3136')
+                .setImage("attachment://leaderboard-1.png")
+            
+            interaction.followUp({ embeds: [emb], files: [attachment] })
+         
+            // let emb = new MessageEmbed()
+            //     .setTitle('Leaderboard')
+            //     .setDescription(`***1 - 10 Users*** **leaderboard**\n\`\`\`py\n${a.toString().replaceAll(',', '\n')}\n\`\`\``)
+            //     .setColor('#2f3136')
          
          
-            let emb2 = new MessageEmbed()
-                .setTitle('Leaderboard')
-                .setDescription(`***10 - 20 Users*** **leaderboard**\n\`\`\`py\n${b.toString().replaceAll(',', '\n')}\n\`\`\``)
-                .setColor('#2f3136')
+            // let emb2 = new MessageEmbed()
+            //     .setTitle('Leaderboard')
+            //     .setDescription(`***10 - 20 Users*** **leaderboard**\n\`\`\`py\n${b.toString().replaceAll(',', '\n')}\n\`\`\``)
+            //     .setColor('#2f3136')
 
-            let emb3 = new MessageEmbed()
-                .setTitle('Leaderboard')
-                .setDescription(`***20 - 30 Users*** **leaderboard**\n\`\`\`py\n${c.toString().replaceAll(',', '\n')}\n\`\`\``)
-                .setColor('#2f3136')
+            // let emb3 = new MessageEmbed()
+            //     .setTitle('Leaderboard')
+            //     .setDescription(`***20 - 30 Users*** **leaderboard**\n\`\`\`py\n${c.toString().replaceAll(',', '\n')}\n\`\`\``)
+            //     .setColor('#2f3136')
          
-            let pg = [emb, emb2, emb3]
-            simplydjs.embedPages(client, interaction, pg, { slash: true })
+            // let pg = [emb, emb2, emb3]
+            // simplydjs.embedPages(client, interaction, pg, { slash: true })
         })
     }
 };
--- commands/info/Leveling System/leaderBoard.js
+++ commands/info/Leveling System/leaderBoard.js
@@ -1,6 +1,7 @@
-const { Client, Message, MessageActionRow, MessageButton, MessageEmbed, MessageSelectMenu } = require('discord.js');
+const { Client, Message, MessageActionRow, MessageButton, MessageEmbed, MessageAttachment, MessageSelectMenu } = require('discord.js');
 const simplydjs = require("simply-djs")
 const config = require('../../../config.json')
+const canvas = require('discord-canvas')
 
 module.exports = {
     name: 'leaderboard',
@@ -18,39 +19,62 @@ module.exports = {
 
     run: async (client, message, args) => {
       if(message.channel.id !== config.levelsChannel) return;
-        await xp.leaderboard(client, message.guild.id, 45).then(board => {
+        await xp.leaderboard(client, message.guild.id, 45).then(async board => {
             let a = []
             let b = []
-						let c = []
+			let c = []
          
             board.forEach(user => {
-                if (user.position <= 10) {
-                    a.push(`@ ${user.username}   • •   'Lvl: ${user.level}'\n--------------------------------------`)
+                let u = message.guild.members.cache.find((x) => x.id === user.userID)
+                if (user.position <= 11) {
+                    a.push({
+                        "icon": `${u.displayAvatarURL({ format: 'png' })}`,
+                        "data": `${user.username}   • •   LVL: ${user.level}`
+                    })
                 } else if (user.position > 10 && user.position <= 20) {
-                    b.push(`@ ${user.username}   • •   'Lvl: ${user.level}'\n--------------------------------------`)
+                    b.push({
+                        "icon": `${u.displayAvatarURL({ format: 'png' })}`,
+                        "data": `${user.username}   • •   LVL: ${user.level}`
+                    })
                 } else if (user.position > 20 && user.position <= 30) {
-                    c.push(`@ ${user.username}   • •   'Lvl: ${user.level}'\n--------------------------------------`)
+                    c.push({
+                        "icon": `${u.displayAvatarURL({ format: 'png' })}`,
+                        "data": `${user.username}   • •   LVL: ${user.level}`
+                    })
                 }
             })
+
+            console.log(a)
+
+            const image = new canvas.Leaderboard()
+                .setData(a)
+                .setAvatars(true)
+                .toAttachment()
+
+            const attachment = new MessageAttachment((await image).toBuffer(), 'leaderboard-1.png');
+
          
             let emb = new MessageEmbed()
-                .setTitle('Leaderboard')
-                .setDescription(`***1 - 10 Users*** **leaderboard**\n\`\`\`py\n${a.toString().replaceAll(',', '\n')}\n\`\`\``)
+                .setTitle(`${message.guild.name}'s Leaderboard`)
+                .setDescription(`Full Leaderboard top 100 users - *Coming Soon*`)
                 .setColor('#2f3136')
+                .setImage("attachment://leaderboard-1.png")
+            
+            message.reply({ embeds: [emb], files: [attachment], allowedMentions: { repliedUser: false } })
          
          
-            let emb2 = new MessageEmbed()
-                .setTitle('Leaderboard')
-                .setDescription(`***10 - 20 Users*** **leaderboard**\n\`\`\`py\n${b.toString().replaceAll(',', '\n')}\n\`\`\``)
-                .setColor('#2f3136')
+            // let emb2 = new MessageEmbed()
+            //     .setTitle('Leaderboard')
+            //     .setDescription(`***10 - 20 Users*** **leaderboard**\n\`\`\`py\n${b.toString().replaceAll(',', '\n')}\n\`\`\``)
+            //     .setColor('#2f3136')
 
-            let emb3 = new MessageEmbed()
-                .setTitle('Leaderboard')
-                .setDescription(`***20 - 30 Users*** **leaderboard**\n\`\`\`py\n${c.toString().replaceAll(',', '\n')}\n\`\`\``)
-                .setColor('#2f3136')
+            // let emb3 = new MessageEmbed()
+            //     .setTitle('Leaderboard')
+            //     .setDescription(`***20 - 30 Users*** **leaderboard**\n\`\`\`py\n${c.toString().replaceAll(',', '\n')}\n\`\`\``)
+            //     .setColor('#2f3136')
          
-            let pg = [emb, emb2, emb3]
-            simplydjs.embedPages(client, message, pg, { slash: false });
+            // let pg = [emb, emb2, emb3]
+            // simplydjs.embedPages(client, message, pg, { slash: false });
         })
     }
 };
