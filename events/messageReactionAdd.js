ed7e27a6ef0aac90b8e1b759dfcb86584c49542f
Reubz <reugeorac@gmail.com>
2023-03-14 19:17:13 +0530

    Added Dynamic Starboard Channel

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -1,15 +1,18 @@
 const simplydjs = require('simply-djs');
 const client = require("../index");
+const { starboardChannel } = require('../config.json');
 
 // Simply - DJS --->> Starboard Command
 
-client.on("messageReactionAdd", async (message) => {
-    simplydjs.starboard(client, message, {
+client.on("messageReactionAdd", async (reaction) => {
+    // if(reaction.guild.me.permissionsIn(reaction.message.channelId).has("MANAGE_MESSAGES"))
+    
+    simplydjs.starboard(client, reaction, {
         event: "messageReactionAdd",
-        chid: "855681042773704714",
+        chid: `${starboardChannel}`,
         embedColor: "2F3136",
         min: 5,
         credit: false,
         embedFoot: "Starboard - Reubz Bot",
-    });
+    }).catch((e) => {})
 })
--- events/messageReactionRemove.js
+++ events/messageReactionRemove.js
@@ -1,12 +1,16 @@
 const simplydjs = require('simply-djs');
 const client = require("../index");
+const { starboardChannel } = require('../config.json');
+
 
 // Simply - DJS --->> Starboard Command
 
 client.on("messageReactionRemove", async (message) => {
+    // if(reaction.guild.me.permissionsIn(reaction.message.channelId).has("MANAGE_MESSAGES"))
+
     simplydjs.starboard(client, message, {
         event: "messageReactionRemove",
-        chid: "855681042773704714",
+        chid: `${starboardChannel}`,
         embedColor: "2F3136",
         min: 5,
         credit: false,
