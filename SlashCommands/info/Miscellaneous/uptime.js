a37399a460d8d8f922736cc7e6e228ab619cdfe6
Reubz <reugeorac@gmail.com>
2023-05-21 00:12:27 +0530

    cleaned code

--- SlashCommands/info/Miscellaneous/uptime.js
+++ SlashCommands/info/Miscellaneous/uptime.js
@@ -3,14 +3,14 @@ const { Client, CommandInteraction, MessageActionRow, MessageButton } = require(
 module.exports = {
     name: "uptime",
     description: "See Bot's uptime",
-    
+
     /**
      * @param {Client} client
      * @param {CommandInteraction} interaction
      * @param {String[]} args
      */
 
-    run: async(client, interaction, args) => {
+    run: async (client, interaction, args) => {
         let totalSeconds = client.uptime / 1000;
         let days = Math.floor(totalSeconds / 86400);
         totalSeconds %= 86400;
@@ -19,6 +19,6 @@ module.exports = {
         let minutes = Math.floor(totalSeconds / 60);
         let seconds = Math.floor(totalSeconds % 60);
 
-        interaction.followUp({content: `<a:online:891575878088269824> • ${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds `})
+        interaction.followUp({ content: `<a:online:891575878088269824> • ${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds ` })
     }
 }
