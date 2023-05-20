d00293130c14d31ffd20e9e860f19deefb93369f
Reubz <reugeorac@gmail.com>
2023-05-21 00:09:00 +0530

    Updated Ping command to not hit the rate limit

--- SlashCommands/info/Miscellaneous/ping.js
+++ SlashCommands/info/Miscellaneous/ping.js
@@ -3,38 +3,28 @@ const { Client, CommandInteraction, MessageEmbed } = require("discord.js")
 module.exports = {
     name: "ping",
     description: "See the bots ping",
-    
+
     /**
      * @param {Client} client
      * @param {CommandInteraction} interaction 
      * @param {String[]} args
      */
 
-    run: async(client, interaction, args) => {
-        let pingEmoji 
-        let date = Date.now()
-        await interaction.editReply({content: `Pinging`});
-        await interaction.editReply({content: `Pinging.`})
-        await interaction.editReply({content: `Pinging..`})
-        await interaction.editReply({content: `Pinging...`})
-        await interaction.editReply({content: `Pinging..`})
-        await interaction.editReply({content: `Pinging.`})
+    run: async (client, interaction, args) => {
+        await interaction.editReply({ content: `Pinging` });
+        await interaction.editReply({ content: `Pinging.` });
+        await interaction.editReply({ content: `Pinging..` });
+        await interaction.editReply({ content: `Pinging...` });
 
-            if(client.ws.ping > 100 ) {
-                pingEmoji = `🔴`
-            }
-            if(client.ws.ping <= 100 ) {
-                pingEmoji = `🟡`
-            }
-            if(client.ws.ping <= 50) {
-                pingEmoji = `🟢`
-            }
+        let ping = client.ws.ping;
+        let pingEmoji = `🔴`;
+        if (ping <= 100) pingEmoji = `🟡`;
+        if (ping <= 50) pingEmoji = `🟢`;
 
-            
         setTimeout(() => {
-            interaction.editReply({ content: `${pingEmoji} • **${client.ws.ping}ms**` });
+            interaction.editReply({ content: `${pingEmoji} • **${ping}ms**` });
         }, 100)
-        
+
     }
 }
 
